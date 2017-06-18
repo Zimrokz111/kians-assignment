23a4e6ed8c247fec370480a2523bfc7cdb2698c2
Nishan Mozumder <arosh019@gmail.com>
2017-06-18 10:47:56 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// Author's Js..
+
+//Smooth scroll Js
+$(document).ready(
+		
+	function() {
+		
+	$("html").niceScroll();
+		
+} );
+
+//Date Picker
+
+
+//Star rating
+
+$('.rating').addRating({
+  max: 5
+});
+
+$('.rating2').addRating({
+  max: 5
+});
